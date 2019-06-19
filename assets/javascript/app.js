0da38937b27df5226a710af272e00d64e126f7d1
Chris Duong <ctd4wa@virginia.edu>
2019-06-19 14:12:38 -0400

    pseudocoding

---
+++ assets/javascript/app.js
@@ -0,0 +1,3 @@
+// start with Start Button page
+// when Start is clicked, questions&answers appear and timer begins
+// x many questions, with answers in the format of radio buttons(html input or bootstrap button ids)
