c0a2347be698632a6d19cf354ba6b2dd1b49d36c
Chris Duong <ctd4wa@virginia.edu>
2019-06-19 21:59:06 -0400

    populating questions array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,22 @@
+$(document).ready(function(){
+    var qna = [
+        {
+            question: "What was the first Pokémon designed?",
+            choices: ["Pikachu", "Ditto", "Charmander", "Rhydon"],
+            answer: 3,
+            image: "https://66.media.tumblr.com/d0c9caf64827981e3cc44c6a9981d61f/tumblr_poivo4iwfq1sgge6vo1_640.gif"
+        },
+        {
+            question: "According to the National Pokédex, which Pokémon is classified as number 25?",
+            choices: ["Pidgey,", "Pikachu", "Sandshrew", "Vulpix"],
+            answer: 0,
+            image: "https://media3.giphy.com/media/39GAXpLVKvYRO/giphy.gif?cid=790b76115d0ae83241474d2e63f9f3fc&rid=giphy.gif"
+        },
+        {
+            question:
+        }
+    ]
+})
 // start with Start Button page
 // when Start is clicked, questions&answers appear and timer begins
 // How do you have an individual container for the Start button and then hide it to begin the game?
