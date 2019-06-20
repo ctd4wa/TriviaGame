6146330e1a0d943a0eb154602b1ab9a8a1786f59
Chris Duong <ctd4wa@virginia.edu>
2019-06-19 23:05:22 -0400

    naming variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,13 +9,41 @@ $(document).ready(function(){
         {
             question: "According to the National Pokédex, which Pokémon is classified as number 25?",
             choices: ["Pidgey,", "Pikachu", "Sandshrew", "Vulpix"],
-            answer: 0,
+            answer: 1,
             image: "https://media3.giphy.com/media/39GAXpLVKvYRO/giphy.gif?cid=790b76115d0ae83241474d2e63f9f3fc&rid=giphy.gif"
         },
         {
-            question:
-        }
+            question: "Pokémon Red featured which Pokémon on its cover?",
+            choices: ["Charizard", "Vulpix", "Moltres", "Charmander"],
+            answer: 0,
+            image: "https://media0.giphy.com/media/4tY9mWimjpD9e/giphy.gif?cid=790b76115d0af4477473576c51a14b82&rid=giphy.gif"
+        },
+        {
+            question: "The names of the Professors featured in the Pokémon console games are based off of what?",
+            choices: ["flowers", "Japanese cuisine", "trees", "teas"],
+            answer: 2,
+            image: "https://media.giphy.com/media/IiA6yfcjtVSsU/giphy.gif"
+        },
     ]
+    var correctCount = 0;
+    var incorrectCount = 0;
+    var blankCount = 0;
+    var timer = 0;
+    var userGuess = "";
+    var intervalId;
+    var QuestionCount = qna.length;
+    var choice;
+
+
+
+    $("#start").on("click", function(){
+        $("#start").hide();
+        showQuestion();
+        startTimer();
+        
+            
+        }
+    })
 })
 // start with Start Button page
 // when Start is clicked, questions&answers appear and timer begins
