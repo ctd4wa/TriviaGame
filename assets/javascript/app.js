aa589962942e394856b5e4f63a9e7c41df4efb19
Chris Duong <ctd4wa@virginia.edu>
2019-06-19 21:37:55 -0400

    finishing the HTML layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
 // start with Start Button page
 // when Start is clicked, questions&answers appear and timer begins
+// How do you have an individual container for the Start button and then hide it to begin the game?
+// Once Start is clicked, the game begins and the timer runs.
+// Show timer and set it to 30 seconds to decrease
 // x many questions, with answers in the format of radio buttons(html input or bootstrap button ids)
+//
